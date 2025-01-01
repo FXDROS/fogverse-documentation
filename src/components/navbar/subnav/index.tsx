2d50884f4ccde5a88daa9f0a034e5b816470f992
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-01 18:21:10 +0700

    fix: remove focus when main title is clicked

--- src/components/navbar/subnav/index.tsx
+++ src/components/navbar/subnav/index.tsx
@@ -6,10 +6,14 @@ const SubNav: React.FC<subNavInterface> = ({ contentList }) => {
   const [selectedSub, setSelectedSub] = useState("");
 
   return (
-    <div className="text-base w-fit">
+    <div className="text-base w-fit overflow-auto fixed">
       {contentList?.map((content, index) => (
         <div key={index} className="mb-4">
-          <a className="font-bold" href={content.path}>
+          <a
+            className="font-bold"
+            href={content.path}
+            onClick={() => setSelectedSub(content.topic)}
+          >
             {content.topic}
           </a>
           {content.content.map((subContent, subIndex) => (
