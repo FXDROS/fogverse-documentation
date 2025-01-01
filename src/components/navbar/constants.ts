4f17935b34b93627db6802c362688d9838ffbce6
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-01 08:56:18 +0700

    chore: add subnav list for documentation menu

--- src/components/navbar/constants.ts
+++ src/components/navbar/constants.ts
@@ -1,18 +1,75 @@
 export const NAVIGATION = [
-    {
-        "name" : "QUICK START",
-        "path" : "/quickstart"
-    },
-    {
-        "name" : "DOCUMENTATION",
-        "path" : "/documentation"
-    },
-    {
-        "name" : "COMMUNITY",
-        "path" : "/community"
-    },
-    {
-        "name" : "GITHUB",
-        "path" : "https://github.com/FXDROS/fogverse"
-    }
-]
+  {
+    name: "QUICKSTART",
+    path: "/quickstart",
+  },
+  {
+    name: "DOCUMENTATION",
+    path: "/documentation",
+  },
+  {
+    name: "COMMUNITY",
+    path: "/community",
+  },
+  {
+    name: "GITHUB",
+    path: "https://github.com/FXDROS/fogverse",
+  },
+];
+
+export const DOCUMENTATION = [
+  {
+    topic: "Getting started",
+    path: "#getting-started",
+    content: [
+      {
+        title: "Introduction",
+        path: "#introduction",
+      },
+      {
+        title: "Installation",
+        path: "#installation",
+      },
+      {
+        title: "FogVerse quickstart",
+        path: "#quickstart",
+      },
+    ],
+  },
+  {
+    topic: "FogVerse basics",
+    path: "#fogverse-basics",
+    content: [
+      {
+        title: "Producer",
+        path: "#producer",
+      },
+      {
+        title: "Consumer",
+        path: "#consumer",
+      },
+      {
+        title: "Storage",
+        path: "#storage",
+      },
+      {
+        title: "Logging",
+        path: "#logging",
+      },
+      {
+        title: "OpenCV consumer",
+        path: "#opencv-consumer",
+      },
+    ],
+  },
+  {
+    topic: "FogVerse advance",
+    path: "#fogverse-advance",
+    content: [
+      {
+        title: "Auto scaling",
+        path: "#auto-scaling",
+      },
+    ],
+  },
+];
