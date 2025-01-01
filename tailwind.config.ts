1acefa0a41d13cf6c5e5ae0a8cfb6d3fab4e04d3
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-01 08:53:32 +0700

    chore: add new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,17 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primaryblue: "var(--primaryblue)"
+        primaryblue: "var(--primaryblue)",
+        primaryred: "var(--primaryred)",
       },
       fontSize: {
-        'xs': '.75rem', // 12px
-        'sm': ['.875rem', '1.25rem'], // 14px
-        'base': ['1.125rem', '1.5rem'], // 18px
-        'lg': ['1.25rem', '1.75rem'], // 20px
-        'very-lg': ['1.5rem', '2rem'],// 24px
-        'semi-xl': ['2rem', '2.5rem'], // 32px
-        'xl' : ['3rem', '3.5rem'], //48px
-        'xxl': ['4rem', '4.5rem'], // 64px
+        xs: ".75rem", // 12px
+        sm: [".875rem", "1.25rem"], // 14px
+        base: ["1.125rem", "1.5rem"], // 18px
+        lg: ["1.25rem", "1.75rem"], // 20px
+        "very-lg": ["1.5rem", "2rem"], // 24px
+        "semi-xl": ["2rem", "2.5rem"], // 32px
+        xl: ["3rem", "3.5rem"], //48px
+        xxl: ["4rem", "4.5rem"], // 64px
       },
     },
   },
