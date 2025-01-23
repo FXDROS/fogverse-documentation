4ccb1a3de86998fa1191a3ec3408d5c0943f7db2
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-23 21:32:30 +0900

    feat: add fogverse basics

--- src/components/pages/documentation/index.tsx
+++ src/components/pages/documentation/index.tsx
@@ -1,11 +1,13 @@
 import DocumentationLayout from "@/components/commons/documentation-layout";
 import { DOCUMENTATION } from "@/components/navbar/constants";
 import Introduction from "./introduction";
+import FogverseBasics from "./fogverse-basics";
 
 const DocumentationPage = () => {
   return (
     <DocumentationLayout contentList={DOCUMENTATION}>
       <Introduction />
+      <FogverseBasics />
     </DocumentationLayout>
   );
 };
