40f0644eaf75c3b0c943a7fbc83025051d0290ee
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-26 23:42:53 +0900

    chore: create tag component

---
+++ src/components/commons/tags/index.tsx
@@ -0,0 +1,27 @@
+export const Required = () => {
+  return (
+    <span className="italic font-bold text-primaryred">&nbsp;[REQUIRED]</span>
+  );
+};
+
+export const RequireImplementation = () => {
+  return (
+    <span className="italic font-bold text-primaryred">
+      &nbsp;[REQUIRE_IMPLEMENTATION]
+    </span>
+  );
+};
+
+export const EmptyFunction = () => {
+  return (
+    <span className="italic font-bold text-primaryblue">
+      &nbsp;[EMPTY_FUNCTION]
+    </span>
+  );
+};
+
+export const AsyncFunction = () => {
+  return (
+    <span className="italic font-bold text-primaryblue">&nbsp;[ASYNC]</span>
+  );
+};
