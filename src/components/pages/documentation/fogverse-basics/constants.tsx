cb4044411f0330a8d0fc37c3a7c2682665c57dbd
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-01-27 17:58:02 +0900

    feat: add consumer attributes documentation

--- src/components/pages/documentation/fogverse-basics/constants.tsx
+++ src/components/pages/documentation/fogverse-basics/constants.tsx
@@ -1,14 +1,82 @@
 export const PRODUCER = {
   initialization: `from fogverse import Producer
-from dotenv import load_dotenv
 
-load_dotenv()
 class MyProducer(Producer):
-	def __init__(self, loop=None):
+	def __init__(self):
 		self.producer_topic = ''            # message topic
         self.producer_servers = ['']        # kafka servers
         auto_decode = False                 # decode every received message
         auto_encode = False                 # encode every sent message
         encode_encoding = ''                # encoding type (jpg, png, etc.)
-		Producer.__init__(self, loop=loop)`,
+		Producer.__init__(self)`,
+
+  base_functions: `    async def start_producer(self):
+        # TODO: Implement code if needed
+        await super().start_producer()
+
+    async def send(self, data, topic=None, key=None, headers=None,
+                   callback=None):
+        # TODO: Implement code if needed
+        await super().send(data, topic=topic, key=key,
+                            headers=headers, callback=callback)
+
+    async def close_producer(self):
+        # TODO: Implement code if needed
+        await super().close_producer()
+
+    async def receive(self):
+        # TODO: Implement code`,
 };
+
+export const CONSUMER = {
+  initialization: `from fogverse import Consumer
+
+    class MyConsumer(Consumer):
+        def __init__(self):
+            self.consumer_topic = ['']          # message topic
+            self.consumer_servers = ['']        # kafka servers
+            auto_decode = False                 # decode every received message
+            auto_encode = False                 # encode every sent message
+            encode_encoding = ''                # encoding type (jpg, png, etc.)
+            Consumer.__init__(self)`,
+
+  base_functions: `    async def start_producer(self):
+            # TODO: Implement code if needed
+            await super().start_producer()
+
+        async def send(self, data, topic=None, key=None, headers=None,
+                        callback=None):
+            # TODO: Implement code if needed
+            await super().send(data, topic=topic, key=key,
+                                headers=headers, callback=callback)
+
+        async def close_producer(self):
+            # TODO: Implement code if needed
+            await super().close_producer()
+
+        async def receive(self):
+            # TODO: Implement code`,
+};
+
+/*
+export const ABSTRACT_CONSUMER = {
+  start_consumer: `async def start_consumer(self):
+    pass`,
+
+  receive_error: `def receive_error(self, *args, **kwargs):
+    pass`,
+};
+
+export const ABSTRACT_PRODUCER = {
+  start_producer: `async def start_producer(self):
+    logger = getattr(self, '_log', None)
+    if isinstance(logger, FogVerseLogging):
+        logger.std_log('Config: %s', self._producer_conf)
+        if getattr(self, 'producer_topic', None) is not None:
+            logger.std_log('Topic: %s', self.producer_topic)
+    await self.producer.start()`,
+
+  receive_error: `def receive_error(self, *args, **kwargs):
+      pass`,
+};
+*/
--- src/components/pages/documentation/fogverse-basics/consumer.tsx
+++ src/components/pages/documentation/fogverse-basics/consumer.tsx
@@ -1,10 +1,73 @@
+import CodeSnippet, { SmallCode } from "@/components/commons/code-snippet";
+import { CONSUMER } from "./constants";
+import UnorderedList from "@/components/commons/unordered-list";
+import { Required } from "@/components/commons/tags";
+
 const Consumer = () => {
   return (
     <div className="mt-12">
       <div id="consumer" className="text-very-lg font-bold my-4">
         Consumer
       </div>
-      <div className="text-base">Consumer is...</div>
+      To consume message from a Kafka topic, a component has to extend the
+      <SmallCode code="Consumer()" />
+      class. Just like a<SmallCode code="Producer()" /> class, it is just and
+      empty class that extends the
+      <SmallCode code="AbstractProducer()" />
+      class, the
+      <SmallCode code="AIOKafkaConsumer()" />
+      class, and the
+      <SmallCode code="Runnable()" />
+      class.
+      <div className="text-lg font-semibold my-4">
+        Consumer&apos;s base attributes
+      </div>
+      Just like creating a producer component, when a consumer component is
+      created, there are several attributes that you can declare during the
+      <SmallCode code="Consumer()" />
+      class initialization, and some of them are required.
+      <CodeSnippet language="python" code={CONSUMER.initialization} />
+      <div className="mt-4 ml-4">
+        <UnorderedList>
+          <SmallCode code="consumer_topic: str[]" />
+          to declare from which topics your consumer will receive the message.
+          This attribute accepts string or array of strings. You can also set
+          the value of this attribute through environment variable with
+          <SmallCode code="CONSUMER_TOPIC" />
+          as the key.
+          <Required />
+        </UnorderedList>
+        <UnorderedList>
+          <SmallCode code="consumer_servers: str[]" />
+          to declare from which Kafka servers your consumer will receive the
+          message. This attribute accepts string or array of strings. You can
+          also set the value of this attribute through environment variable with
+          <SmallCode code="CONSUMER_SERVERS" />
+          as the key.
+          <Required />
+        </UnorderedList>
+        <UnorderedList>
+          <SmallCode code="auto_decode: bool" />
+          to decode received data (file) from bytes to numpy array.
+        </UnorderedList>
+        <UnorderedList>
+          <SmallCode code="auto_encode: bool" />
+          to encode data according to the encoding type.
+        </UnorderedList>
+        <UnorderedList>
+          <SmallCode code="encode_encoding: str" />
+          to declare type of encoding that is being used for the corresponding
+          message e.g.
+          <SmallCode code="jpg" />
+          and
+          <SmallCode code="png" />.
+        </UnorderedList>
+      </div>
+      <div>
+        <div className="text-lg font-semibold my-4">
+          Consumer&apos;s built-in functions
+        </div>
+      </div>
     </div>
   );
 };
